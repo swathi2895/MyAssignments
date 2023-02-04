a8c8017f90ffb67489368c3001c5f2e9449a6e67
swathi2895 <121571228+swathi2895@users.noreply.github.com>
2023-02-04 22:12:25 +0530

    Add files via upload
    Day 1 Assignments - Two Wheeler and Mobile

---
+++ day1/Mobile.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Mobile {
+public static void main(String[] args) {
+	String mobileBrandName="OnePlus";
+	char mobileLogo ='1';
+	short noOfMobilepiece= 100; ;
+	int modelNumber=7;
+	long mobileImeiNumber=8909809809l;
+	float mobilePrice= 47000.5f;
+	boolean isDamaged=false;
+	System.out.println("Mobile brand name is "+' '+mobileBrandName);
+	System.out.println("Mobile logo is "+' '+mobileLogo);
+	System.out.println("Model number is" +' '+modelNumber);
+	System.out.println("mobile Imei number is"+' '+mobileImeiNumber);
+	System.out.println("Mobile price is "+' '+mobilePrice);
+	System.out.println("Is mobile damaged?"+' '+isDamaged);
+}
+}
---
+++ day1/TwoWheeler.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class TwoWheeler {
+public static void main(String[] args) {
+	int noOfWheels=4;
+	short noOfMirrors= -7;
+	long chassisNumber=243247834l;
+	boolean isPunctured=true;
+	String bikeName="Royal Enfield";
+	double runningKm=4353.765;
+	System.out.println("No of wheels" +' '+ noOfWheels);
+	System.out.println("Chassis Number is" +' '+ noOfMirrors);
+	System.out.println("Is vehicle punctured" + ' '+ isPunctured);
+	System.out.println("Bike name is"+ ' '+bikeName);
+	System.out.println("Running km is" +' '+runningKm);
+}
+}
