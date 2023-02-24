04d03db88e78bf8968456a45334719c14594aba9
swathi2895 <121571228+swathi2895@users.noreply.github.com>
2023-02-25 01:47:33 +0530

    Add files via upload

---
+++ Day2/Day2Abstration/MultipleLanguage.java
@@ -0,0 +1,12 @@
+package week3.Day2Abstration;
+
+public abstract class MultipleLanguage implements TestTool,Language {
+public void python() {
+	System.out.println("Python is a programming language");
+
+}
+public abstract void ruby();
+	
+
+
+}
